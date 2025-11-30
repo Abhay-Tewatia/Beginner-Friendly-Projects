a5fa6fecb64c8a945e4bce17d3f596e8015857ff
Abhay-Tewatia <abhaytewtia342@gmail.com>
2025-12-01 00:59:29 +0530

    Click kisi chhote element par hota hai, par event upar parent tak travel karta hai. Isi ko event bubbling bolte hain.

---
+++ 5. Bubble Game/script.js
@@ -0,0 +1,57 @@
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+
+// jispe clcik kroge wo elemnt per event raise hoga ,aur event listner na milne par element ke parent par listener dhundhega, waha bhi naa milne par event parent ke parent ke parent par listner dhundega
+
+function increaseScore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+function getNewHit(){
+     hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = hitrn;
+
+
+}
+
+function makeBubble() {
+  var clutter = " ";
+
+  for (var i = 1; i <= 100; i++) {
+    var rn = Math.floor(Math.random() * 10);
+    clutter += ` <div class="bubble">${rn}</div>`;
+  }
+  document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+function runTimer() {
+  var timerint = setInterval(function () {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timervalue").textContent = timer;
+    } else {
+      clearInterval(timerint);
+      document.querySelector("#pbtm").innerHTML = ` <h1> Game Over 😭 </h1>
+       `;
+    }
+  }, 1000);
+}
+
+document.querySelector("#pbtm").addEventListener("click", function(dets){
+    var clicknum = Number(dets.target.textContent);
+    if(clicknum === hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+});
+
+
+
+
+getNewHit();
+runTimer();
+makeBubble();
+// increaseScore();
